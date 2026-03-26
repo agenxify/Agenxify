47f1b127021e42dc9f986d1e57902769cc8e9cb4
Agenxify <growthedgetube@gmail.com>
2026-03-27 01:23:37 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist/**', '.eslintrc.cjs'],
+    ignores: ['dist/**', '.eslintrc.cjs', 'api/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
